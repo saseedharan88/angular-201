367a63372879213f9b2f0e4445d11a963b650242
saseedharan j <saseedharanj@A2ML28619M.local>
2019-02-16 17:16:40 +0530

    New theming.

--- anytime-library/src/app/app-routing.module.ts
+++ anytime-library/src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
-const routes: Routes = [];
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BooksComponent } from './books/books.component';
+const routes: Routes = [
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+    { path: 'books', component: BooksComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- anytime-library/src/app/app.module.ts
+++ anytime-library/src/app/app.module.ts
@@ -6,12 +6,33 @@ import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NavComponent } from './nav/nav.component';
 import { LayoutModule } from '@angular/cdk/layout';
-import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule } from '@angular/material';
+import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, MatListModule, MatMenuModule, MatTabsModule, MatInputModule } from '@angular/material';
+import { AdminModule } from './admin/admin.module';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { BooksComponent } from './books/books.component';
+import { BookDetailComponent } from './book-detail/book-detail.component';
+import { PrimaryNavComponent } from './primary-nav/primary-nav.component';
+import { LayoutComponent } from './layout/layout.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+import { SearchComponent } from './navigation/search/search.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavComponent
+    NavComponent,
+    LoginComponent,
+    RegisterComponent,
+    BooksComponent,
+    BookDetailComponent,
+    PrimaryNavComponent,
+    LayoutComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
@@ -22,7 +43,12 @@ import { MatToolbarModule, MatButtonModule, MatSidenavModule, MatIconModule, Mat
     MatButtonModule,
     MatSidenavModule,
     MatIconModule,
-    MatListModule
+    MatListModule,
+    MatMenuModule,
+    MatTabsModule,
+    MatInputModule,
+    AdminModule,
+    FlexLayoutModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
